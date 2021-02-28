915f1f71e2f63ba984b3329d7e678c1d61198b5c
SujithGunasekaran <mydevelopstar@gmail.com>
2021-02-28 10:44:16 +0530

    New Folder created for apollo queries in client

---
+++ apollo/queries/index.js
@@ -0,0 +1,18 @@
+import { gql } from '@apollo/client';
+
+
+export const GET_PORTFOLIOBYID = gql`
+    query Portfolio($id : ID) {
+        portfolio( id : $id ) {
+            _id,
+            title,
+            company,
+            companyWebsite,
+            location,
+            jobTitle,
+            description,
+            startDate,
+            endDate
+        }
+    }
+`;
