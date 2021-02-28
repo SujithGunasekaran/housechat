de09f90a365e8cb9b1391542e180f0c4c50078e5
SujithGunasekaran <mydevelopstar@gmail.com>
2021-02-28 20:35:20 +0530

    43. Created new Folder actions for query and mutation

---
+++ apollo/actions/index.js
@@ -0,0 +1,28 @@
+import { useMutation, useQuery } from '@apollo/client';
+import { GET_PORTFOLIOS, CREATE_PORTFOLIO, UPDATE_PORTFOLIO, DELETE_PORTFOLIO } from '../queries';
+
+
+export const useGetPortfolios = () => useQuery(GET_PORTFOLIOS);
+
+export const useUpdatePortfolios = () => useMutation(UPDATE_PORTFOLIO);
+
+export const useDeletePortfolios = () => useMutation(DELETE_PORTFOLIO, {
+    update(cache, { data: { deletePortfolio } }) {
+        const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
+        const newPortfolios = portfolios.filter((portfolioID) => portfolioID._id !== deletePortfolio);
+        cache.writeQuery({
+            query: GET_PORTFOLIOS,
+            data: { portfolios: newPortfolios }
+        })
+    }
+});
+
+export const useCreatePortfolios = () => useMutation(CREATE_PORTFOLIO, {
+    update(cache, { data: { createPortfolio } }) {
+        const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
+        cache.writeQuery({
+            query: GET_PORTFOLIOS,
+            data: { portfolios: [...portfolios, createPortfolio] }
+        })
+    }
+})
--- pages/Portfolio/index.js
+++ pages/Portfolio/index.js
@@ -1,34 +1,16 @@
 import PortfolioCard from '../../Components/portfolio/PortfolioCard';
 import Link from 'next/link';
-import { useMutation, useQuery } from '@apollo/client';
-import { GET_PORTFOLIOS, CREATE_PORTFOLIO, UPDATE_PORTFOLIO, DELETE_PORTFOLIO } from '../../apollo/queries';
+import { useGetPortfolios, useCreatePortfolios, useUpdatePortfolios, useDeletePortfolios } from '../../apollo/actions';
 import withApollo from '../../hoc/withApollo';
 import { getDataFromTree } from '@apollo/client/react/ssr';
 
 
 const Portfolio = () => {
 
-    const { data } = useQuery(GET_PORTFOLIOS);
-    const [updatePortfolio] = useMutation(UPDATE_PORTFOLIO);
-    const [deletePortfolio] = useMutation(DELETE_PORTFOLIO, {
-        update(cache, { data: { deletePortfolio } }) {
-            const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
-            const newPortfolios = portfolios.filter((portfolioID) => portfolioID._id !== deletePortfolio);
-            cache.writeQuery({
-                query: GET_PORTFOLIOS,
-                data: { portfolios: newPortfolios }
-            })
-        }
-    });
-    const [createPortfolio] = useMutation(CREATE_PORTFOLIO, {
-        update(cache, { data: { createPortfolio } }) {
-            const { portfolios } = cache.readQuery({ query: GET_PORTFOLIOS });
-            cache.writeQuery({
-                query: GET_PORTFOLIOS,
-                data: { portfolios: [...portfolios, createPortfolio] }
-            })
-        }
-    })
+    const { data } = useGetPortfolios();
+    const [updatePortfolio] = useUpdatePortfolios();
+    const [deletePortfolio] = useDeletePortfolios();
+    const [createPortfolio] = useCreatePortfolios();
 
     const portfolios = data ? data.portfolios : [];
 
@@ -44,7 +26,6 @@ const Portfolio = () => {
                     <button onClick={createPortfolio} >CreatePortfolio</button>
                     <div className="row">
                         {
-                            // loading ? <div>Loading....</div> :
                             portfolios.map((portfolioInfo) => (
                                 <div className="col-md-4" key={portfolioInfo._id}>
                                     <Link
