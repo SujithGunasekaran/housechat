962c9f07c1e315fbfcdec0e23a395a71d392845e
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-02 19:56:50 +0530

    53. bcrypt package installed and implemented in schema

--- server/database/model/DB_userModel.js
+++ server/database/model/DB_userModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
@@ -31,6 +32,18 @@ const UserSchema = new Schema({
     },
     info: String,
     createdAt: { type: Date, default: Date.now }
+})
+
+UserSchema.pre('save', function (next) {
+    const user = this;
+    bcrypt.genSalt(10, function (err, salt) {
+        if (err) { return next(err) };
+        bcrypt.hash(user.password, salt, function (err, hash) {
+            if (err) { return next(err) };
+            user.password = hash;
+            next();
+        })
+    })
 })
 
 module.exports = mongoose.model('User', UserSchema);
