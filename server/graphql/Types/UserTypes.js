651afe156c26179a1bce6b53b576dbec5ba624c7
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-02 16:58:27 +0530

    52. userTypes implemented

---
+++ server/graphql/Types/UserTypes.js
@@ -0,0 +1,10 @@
+exports.userTypes = `
+    input signUpInput {
+        avatar : String
+        username : String!
+        name : String
+        email : String!
+        password : String!
+        passwordConfirmation : String!
+    }
+`
