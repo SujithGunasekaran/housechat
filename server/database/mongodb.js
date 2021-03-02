447314a555e03a23abba83f544d3fec6dbd99743
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-02 12:24:27 +0530

    Portfolio imported

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('../config/dev');
 
+const Portfolios = require('../database/model/portfolioModel');
+
 exports.connect = () => {
     mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
         console.log("Mongodb Connected successfully");
