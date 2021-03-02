89e94b796f72dabccc68132e199bd47dd389f6c1
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-02 15:58:29 +0530

    included userCreate index in mongoose connect

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const config = require('../config/dev');
 
-const Portfolios = require('../database/model/DB_portfolioModel');
+require('../database/model/DB_portfolioModel');
+require('../database/model/DB_userModel');
 
 exports.connect = () => {
-    mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
-        console.log("Mongodb Connected successfully");
-    })
+    mongoose.connect(config.mongoURI,
+        {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+            useCreateIndex: true
+        }, () => {
+            console.log("Mongodb Connected successfully");
+        })
 };
--- server/fakeDB/populate.js
+++ server/fakeDB/populate.js
@@ -3,7 +3,12 @@ const mongoose = require('mongoose');
 const config = require('../config/dev');
 const fakeDB = require('./fakeDB');
 
-mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true }, async () => {
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}, async () => {
     console.log("Start populating DB......");
     await fakeDB.populate();
     mongoose.connection.close();
