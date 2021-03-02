3edd6802426599bedfe20eab9d8419fcd98b1b0b
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-02 11:56:56 +0530

    48. Context model implemented

--- server/database/mongodb.js
+++ server/database/mongodb.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('../config/dev');
 
-const portfolio = require('./model/portfolioModel');
-
 exports.connect = () => {
     mongoose.connect(config.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
         console.log("Mongodb Connected successfully");
--- server/graphql/Resolver/PortfolioResolver.js
+++ server/graphql/Resolver/PortfolioResolver.js
@@ -1,64 +1,24 @@
-const Portfolios = require('../../database/model/portfolioModel');
-
-const data = {
-    Portfolio: [
-        {
-            _id: "1",
-            title: 'Job in Netcentric',
-            company: 'Netcentric',
-            companyWebsite: 'https://www.google.com',
-            location: 'Spain, Barcelona',
-            jobTitle: 'Engineer',
-            description: 'Doing something, programing....',
-            startDate: '01/01/2014',
-            endDate: '01/01/2016'
-        },
-        {
-            _id: "2",
-            title: 'Job in Siemens',
-            company: 'Siemens',
-            companyWebsite: 'https://www.google.com',
-            location: 'Slovakia, Kosice',
-            jobTitle: 'Software Engineer',
-            description: 'Responsible for parsing framework for JSON medical data.',
-            startDate: '01/01/2011',
-            endDate: '01/01/2013'
-        },
-        {
-            _id: "3",
-            title: 'Work in USA',
-            company: 'WhoKnows',
-            companyWebsite: 'https://www.google.com',
-            location: 'USA, Montana',
-            jobTitle: 'Full Stack Developer',
-            description: 'Building Responsive and Configurable Website',
-            startDate: '01/01/2010',
-            endDate: '01/01/2011'
-        }
-    ]
-}
-
 
 exports.portfolioQuerys = {
-    portfolio: (root, { id }) => {
-        return Portfolios.findById(id);
+    portfolio: (root, { id }, context) => {
+        return context.models.PortfolioModel.getById(id);
     },
-    portfolios: () => {
-        return Portfolios.find({});
+    portfolios: (root, args, context) => {
+        return context.models.PortfolioModel.getAll();
     }
 }
 
 exports.portfolioMutations = {
-    createPortfolio: async (root, { input }) => {
-        const createdPortfolios = await Portfolios.create(input);
+    createPortfolio: async (root, { input }, context) => {
+        const createdPortfolios = await context.models.PortfolioModel.create(input);
         return createdPortfolios;
     },
-    updatePortfolio: async (root, { id, input }) => {
-        const updatedPortfolio = await Portfolios.findOneAndUpdate({ _id: id }, input, { new: true });
+    updatePortfolio: async (root, { id, input }, context) => {
+        const updatedPortfolio = await context.models.PortfolioModel.findAndUpdate(id, input);
         return updatedPortfolio;
     },
-    deletePortfolio: async (root, { id }) => {
-        const deletedPortfolio = await Portfolios.findOneAndRemove({ _id: id });
+    deletePortfolio: async (root, { id }, context) => {
+        const deletedPortfolio = await context.models.PortfolioModel.findAndDelete(id);
         return deletedPortfolio._id;
     }
 }
