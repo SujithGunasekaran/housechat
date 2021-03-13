0299255fccb8c95f847a9a6370ffd44fb97affa6
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-13 21:37:30 +0530

    Added new folder variables

---
+++ variables/messages.js
@@ -0,0 +1,9 @@
+export const messages = {
+    "Success": {
+        LOGGED_IN: 'You have been successfully registerd, you can login now!'
+    },
+    "Error": {
+        NOT_AUTHENTICATED: 'You need to login to get an access!',
+        NOT_AUTHORIZED: 'You are not authorize to visit this page!',
+    }
+}
