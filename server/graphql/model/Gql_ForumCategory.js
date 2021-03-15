342ba62068c490461da33db3ca4376acc3426720
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-15 11:52:59 +0530

    97. Created new forumCategory model in graphql

---
+++ server/graphql/model/Gql_ForumCategory.js
@@ -0,0 +1,13 @@
+
+class ForumCategory {
+
+    constructor(model) {
+        this.Model = model;
+    }
+
+    getAll() {
+        return this.Model.find({});
+    }
+}
+
+module.exports = ForumCategory;
