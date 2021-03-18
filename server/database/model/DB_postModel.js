c813ec031c9f5712ee4df49ad8132f2f1b483dd4
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-18 12:34:13 +0530

    Created new post model

---
+++ server/database/model/DB_postModel.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const postSchema = new Schema({
+    content: {
+        type: String
+    },
+    slug: {
+        type: String,
+        unique: true,
+        index: true
+    },
+    fullSlug: {
+        type: String,
+        unique: true,
+        index: true
+    },
+    topic: {
+        type: Schema.Types.ObjectId,
+        ref: 'topic'
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    parent: {
+        type: Schema.Types.ObjectId,
+        ref: 'post'
+    },
+    createdAt: {
+        type: Date, default: Date.now
+    }
+})
+
+module.exports = mongoose.model('post', postSchema);
