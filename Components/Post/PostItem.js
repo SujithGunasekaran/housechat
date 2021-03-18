61ebecbfc6539baa130862d3fbda57aa3ac006d0
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-18 20:27:27 +0530

    New Postitem component added

---
+++ Components/Post/PostItem.js
@@ -0,0 +1,58 @@
+import { formatDate } from '../../utils/Function';
+import PersonIcon from '@material-ui/icons/Person';
+
+
+function PostItem({ post }) {
+
+    return (
+        <div>
+            <div className="topic_post_container">
+                <div className="row">
+                    {/* <div className="col-1 col-sm-1 col-md-1">
+                        <PersonIcon className="topic_post_user_profile" />
+                    </div> */}
+                    <div className="col-12 col-sm-12 col-md-12">
+                        <div className="topic_post_user_main_display">
+                            <PersonIcon className="topic_post_user_profile" />
+                            <div className="topic_post_user_info_display">
+                                <div className="topic_post_username">{post.user.username}</div>
+                                {
+                                    post.parent &&
+                                    <div className="topic_post_parent_container">
+                                        <div className="row">
+                                            {/* <div className="col-1 col-sm-1 col-md-1">
+                                                <PersonIcon className="topic_post_user_profile" />
+                                            </div> */}
+                                            <div className="col-12 col-sm-12 col-md-12">
+                                                <div className="topic_post_user_main_display">
+                                                    <PersonIcon className="topic_post_user_profile" />
+                                                    <div className="topic_post_user_info_display">
+                                                        <div className="topic_post_username">{post.parent.user.username}</div>
+                                                        <div className="topic_post_user_answer">{post.parent.content}</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                <div className="topic_post_user_answer">{post.content}</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-6 col-sm-6 col-md-6">
+                        <div className="topic_post_time">Posted : {formatDate(post.createdAt)}</div>
+                    </div>
+                    <div className="col-6 col-sm-6 col-md-6">
+                        <div className="topic_post_reply_container">
+                            <div className="topic_post_reply">Reply</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PostItem;
