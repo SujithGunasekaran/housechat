2c97e070498b7986b240387c2f1995a8942996bb
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-18 20:28:29 +0530

    Topic post data form db displayed in ui

--- pages/forum/Topics/[topicSlug].js
+++ pages/forum/Topics/[topicSlug].js
@@ -1,9 +1,9 @@
-import PersonIcon from '@material-ui/icons/Person';
 import BaseLayout from '../../../layouts/BaseLayout';
 import { useRouter } from 'next/router';
 import { useGetTopicBySlug, useGetPostByTopic } from '../../../apollo/actions';
 import withApollo from '../../../hoc/withApollo';
 import { getDataFromTree } from '@apollo/client/react/ssr';
+import PostItem from '../../../Components/Post/PostItem';
 
 const useInitialData = () => {
 
@@ -23,8 +23,7 @@ const useInitialData = () => {
 function Post() {
 
     const { topicData, postData } = useInitialData();
-    console.log("postData", postData);
-    console.log("topicData", topicData);
+
     return (
         <BaseLayout>
             <div>
@@ -35,87 +34,28 @@ function Post() {
                                 <div className="topic_post_heading">{topicData.title}</div>
                             </div>
                         </div>
-                        <div className="row">
-                            <div className="col-md-9 mx-auto">
-                                <div className="topic_post_container">
-                                    <div className="row">
-                                        <div className="col-1 col-sm-1 col-md-1">
-                                            <PersonIcon className="topic_post_user_profile" />
-                                        </div>
-                                        <div className="col-11 col-sm-11 col-md-11">
-                                            <div className="topic_post_user_info_display">
-                                                <div className="topic_post_username">{topicData.user && topicData.user.username}</div>
-                                                <div className="topic_post_user_answer">{topicData.content}</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-6 col-sm-6 col-md-6">
-                                            <div className="topic_post_time">Posted : 2hr</div>
-                                        </div>
-                                        <div className="col-6 col-sm-6 col-md-6">
-                                            <div className="topic_post_reply_container">
-                                                <div className="topic_post_reply">Reply</div>
-                                            </div>
-                                        </div>
-                                    </div>
+                        {
+                            topicData && topicData._id &&
+                            <div className="row" key={topicData._id}>
+                                <div className="col-md-10">
+                                    <PostItem
+                                        post={topicData}
+                                    />
                                 </div>
                             </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-9 mx-auto">
-                                <div className="topic_post_container">
-                                    <div className="row">
-                                        <div className="col-1 col-sm-1 col-md-1">
-                                            <PersonIcon className="topic_post_user_profile" />
-                                        </div>
-                                        <div className="col-11 col-sm-11 col-md-11">
-                                            <div className="topic_post_user_info_display">
-                                                <div className="topic_post_username">James</div>
-                                                <div className="topic_post_user_answer">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-6 col-sm-6 col-md-6">
-                                            <div className="topic_post_time">Posted : 2hr</div>
-                                        </div>
-                                        <div className="col-6 col-sm-6 col-md-6">
-                                            <div className="topic_post_reply_container">
-                                                <div className="topic_post_reply">Reply</div>
-                                            </div>
-                                        </div>
+                        }
+                        {
+                            postData &&
+                            postData.map((postInfo) => (
+                                <div className="row" key={postInfo._id}>
+                                    <div className="col-md-8">
+                                        <PostItem
+                                            post={postInfo}
+                                        />
                                     </div>
                                 </div>
-                            </div>
-                        </div>
-                        <div className="row">
-                            <div className="col-md-9 mx-auto">
-                                <div className="topic_post_container">
-                                    <div className="row">
-                                        <div className="col-1 col-sm-1 col-md-1">
-                                            <PersonIcon className="topic_post_user_profile" />
-                                        </div>
-                                        <div className="col-11 col-sm-11 col-md-11">
-                                            <div className="topic_post_user_info_display">
-                                                <div className="topic_post_username">James</div>
-                                                <div className="topic_post_user_answer">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div className="row">
-                                        <div className="col-6 col-sm-6 col-md-6">
-                                            <div className="topic_post_time">Posted : 2hr</div>
-                                        </div>
-                                        <div className="col-6 col-sm-6 col-md-6">
-                                            <div className="topic_post_reply_container">
-                                                <div className="topic_post_reply">Reply</div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
+                            ))
+                        }
                     </div>
                 </div>
             </div>
