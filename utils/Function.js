c080d8f661f60e5019516d3b688c8f5b16367063
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-19 14:19:06 +0530

    New moment function added

--- utils/Function.js
+++ utils/Function.js
@@ -3,3 +3,7 @@ import moment from 'moment';
 export const formatDate = (date) => {
     return moment.unix(date / 1000).format('DD/MM/YYYY');
 }
+
+export const fromNow = (date) => {
+    return moment.unix(date / 1000).fromNow();
+}
