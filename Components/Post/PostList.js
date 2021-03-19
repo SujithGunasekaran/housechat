5c28f1cff0cfa153183680cde328657266c29bf0
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-19 14:16:53 +0530

    PostList component added

---
+++ Components/Post/PostList.js
@@ -0,0 +1,34 @@
+import PostItem from './PostItem';
+
+function PostList({ topicData, postData, onReplyOpen, canCreate }) {
+    return (
+        <>
+            {
+                topicData && topicData._id &&
+                <div className="row" key={topicData._id}>
+                    <div className="col-md-10">
+                        <PostItem
+                            post={topicData}
+                        />
+                    </div>
+                </div>
+            }
+            {
+                postData &&
+                postData.map((postInfo) => (
+                    <div className="row" key={postInfo._id}>
+                        <div className="col-md-8">
+                            <PostItem
+                                canCreate={canCreate}
+                                post={postInfo}
+                                onReplyOpen={onReplyOpen}
+                            />
+                        </div>
+                    </div>
+                ))
+            }
+        </>
+    )
+}
+
+export default PostList;
