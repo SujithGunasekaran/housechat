ccdd73c6948b5f97af421bbe93046f643669d046
SujithGunasekaran <mydevelopstar@gmail.com>
2021-03-23 21:19:37 +0530

    Footer Component Updated

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,8 +1,15 @@
+import GitHubIcon from '@material-ui/icons/GitHub';
+import Link from 'next/link';
 
 export default function Footer() {
     return (
         <div className="footer_main_container">
-            Hello
+            <div className="footer_main_icon_container">
+                <a href='https://github.com/SujithGunasekaran' target='_blank' rel='noreference'>
+                    <GitHubIcon className="footer_main_icon" />
+                </a>
+            </div>
+            <div className="footer_main_info">Knowledge Sharing Platform</div>
         </div>
     )
 }
