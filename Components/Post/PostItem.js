0d89d562aead0c6d1bbe098399357535ed01b89c
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-01 20:29:58 +0530

    Postitem component ui updated

--- Components/Post/PostItem.js
+++ Components/Post/PostItem.js
@@ -7,11 +7,38 @@ function PostItem({ post, onReplyOpen, canCreate }) {
         <div>
             <div className="topic_post_container">
                 <div className="row">
-                    {/* <div className="col-1 col-sm-1 col-md-1">
-                        <PersonIcon className="topic_post_user_profile" />
-                    </div> */}
                     <div className="col-12 col-sm-12 col-md-12">
                         <div className="topic_post_user_main_display">
+                            <div className="topic_post_user_info_display">
+                                <div className="hero_topic_avatar_background">
+                                    <PersonIcon className="hero_topic_user_avatar" />
+                                </div>
+                                <div className="topic_post_username">{post.user.username}</div>
+                                <div className="topic_post_time">{post.createdAt && fromNow(post.createdAt)}</div>
+                            </div>
+                            <div className="topic_post_user_content_display">
+                                {
+                                    post.parent &&
+                                    <div className="topic_post_parent_container">
+                                        <div className="row">
+                                            <div className="col-12 col-sm-12 col-md-12">
+                                                <div className="topic_post_user_main_display">
+                                                    <div className="topic_post_parent_user_info_display">
+                                                        <div className="topic_post_username">{post.parent.user.username}</div>
+                                                        <div className="topic_post_time">{post.createdAt && fromNow(post.createdAt)}</div>
+                                                    </div>
+                                                    <div className="topic_post_parent_user_content_display">
+                                                        <div className="topic_post_parent_user_answer">{post.parent.content}</div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                }
+                                <div className="topic_post_user_answer">{post.content}</div>
+                            </div>
+                        </div>
+                        {/* <div className="topic_post_user_main_display">
                             <PersonIcon className="topic_post_user_profile" />
                             <div className="topic_post_user_info_display">
                                 <div className="topic_post_username">{post.user.username}</div>
@@ -19,9 +46,9 @@ function PostItem({ post, onReplyOpen, canCreate }) {
                                     post.parent &&
                                     <div className="topic_post_parent_container">
                                         <div className="row">
-                                            {/* <div className="col-1 col-sm-1 col-md-1">
+                                            <div className="col-1 col-sm-1 col-md-1">
                                                 <PersonIcon className="topic_post_user_profile" />
-                                            </div> */}
+                                            </div>
                                             <div className="col-12 col-sm-12 col-md-12">
                                                 <div className="topic_post_user_main_display">
                                                     <PersonIcon className="topic_post_user_profile" />
@@ -36,15 +63,15 @@ function PostItem({ post, onReplyOpen, canCreate }) {
                                 }
                                 <div className="topic_post_user_answer">{post.content}</div>
                             </div>
-                        </div>
+                        </div> */}
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-6 col-sm-6 col-md-6">
-                        {
+                        {/* {
                             post.createdAt &&
                             <div className="topic_post_time">{fromNow(post.createdAt)}</div>
-                        }
+                        } */}
                     </div>
                     <div className="col-6 col-sm-6 col-md-6">
                         <div className="topic_post_reply_container">
