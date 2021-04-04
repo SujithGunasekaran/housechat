6cbd80df088e95cfbc8122bc5c05422d21774bea
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-04 20:08:14 +0530

    Topic post linking line style added

--- Components/Post/PostList.js
+++ Components/Post/PostList.js
@@ -8,24 +8,41 @@ function PostList(props) {
         <>
             {
                 topicData && topicData._id && currentPage === 1 &&
-                <div className="row" key={topicData._id}>
-                    <div className="col-md-10">
-                        <PostItem
-                            post={topicData}
-                        />
+                <>
+                    <div className="row" key={topicData._id}>
+                        <div className="col-md-10">
+                            <PostItem
+                                post={topicData}
+                            />
+                        </div>
                     </div>
-                </div>
+                    {
+                        postData.length > 0 &&
+                        <div className="row">
+                            <div className="col-md-12">
+                                <div className="topic_post_link_line"></div>
+                            </div>
+                        </div>
+                    }
+                </>
             }
             {
                 postData &&
-                postData.map((postInfo) => (
-                    <div className="row" key={postInfo._id}>
-                        <div className="col-md-10">
-                            <PostItem
-                                canCreate={canCreate}
-                                post={postInfo}
-                                onReplyOpen={onReplyOpen}
-                            />
+                postData.map((postInfo, index) => (
+                    <div key={index}>
+                        <div className="row" key={postInfo._id}>
+                            <div className="col-md-10">
+                                <PostItem
+                                    canCreate={canCreate}
+                                    post={postInfo}
+                                    onReplyOpen={onReplyOpen}
+                                />
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-md-12">
+                                {index < postData.length - 1 && <div className="topic_post_link_line"></div>}
+                            </div>
                         </div>
                     </div>
                 ))
