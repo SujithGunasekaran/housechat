c9cbd77b1dbaa089aa17a26377561d4840870d2c
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-16 21:33:48 +0530

    getUserFollowingList method updated

--- server/graphql/model/Gql_UserFollowingModel.js
+++ server/graphql/model/Gql_UserFollowingModel.js
@@ -6,9 +6,11 @@ class UserFollowingModel extends BaseModel {
 
     async getUserFollowingList() {
         if (this.user) {
-            const followingCount = await this.Model.countDocuments({ userId: this.user._id });
-            const userData = await this.Model.find({ userId: this.user._id }).populate('userFollowingId');
-            return { userData, followingCount };
+            const followingCount = await this.Model.countDocuments({ userInfo: this.user._id });
+            const followersCount = await this.Model.countDocuments({ userFollowingInfo: this.user._id });
+            const userFollowingData = await this.Model.find({ userInfo: this.user._id }).populate('userFollowingInfo');
+            const userFollowersData = await this.Model.find({ userFollowingInfo: this.user._id }).populate('userInfo');
+            return { userFollowingData, userFollowersData, followingCount, followersCount };
         }
         return null;
     }
