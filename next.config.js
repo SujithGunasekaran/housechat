62a63ec215bec43fde89570bade2d35085f697ed
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-20 20:31:45 +0530

    config updated

--- next.config.js
+++ next.config.js
@@ -2,7 +2,5 @@
 const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
-    env: {
-        BASE_URL: dev ? 'http://localhost:3000/graphql' : 'https://housechat.herokuapp.com/graphql'
-    }
+    env: 'Base URL Setup'
 }
