ce2908da36476f581d1915282d7dd8cca80e5e86
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-21 22:32:17 +0530

    Followers component updated and getting data from server

--- Components/Profile/Follower.js
+++ Components/Profile/Follower.js
@@ -1,22 +1,40 @@
+import { useEffect } from 'react';
 import CircularLoading from '../CircularLoading';
 import { useGetUserFollower } from '../../apollo/actions';
+import PersonIcon from '@material-ui/icons/Person';
+
 
 export default function Followers(props) {
 
     const { userId } = props;
 
     // query
-
     const { data, loading, error } = useGetUserFollower(userId);
 
-    const userFollowerData = data?.getUserFollowers?.userFollowersData ?? [];
-
-    console.log("userFollowerData", userFollowerData);
+    const userFollower = data?.getUserFollowers?.userFollowersData ?? [];
 
     return (
         <div>
             { loading && <CircularLoading />}
-            Follower component
+            <div className="follow_container">
+                {
+                    userFollower.length > 0 &&
+                    userFollower.map((userData, index) => (
+                        <div key={index}>
+                            <div className="follow_card_container">
+                                <div className="follow_card_avatar_background">
+                                    <PersonIcon className="follow_card_avatar" />
+                                </div>
+                                <div className="follow_card_user_info">
+                                    <div className="follow_card_user_name">{userData.userInfo.name}</div>
+                                </div>
+                            </div>
+                            { userFollower.length - 1 > index && <hr className="follow_card_hr"></hr>}
+                        </div>
+                    ))
+                }
+                {error && <div></div>}
+            </div>
         </div>
     )
 }
