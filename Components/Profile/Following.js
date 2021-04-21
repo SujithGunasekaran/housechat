c18f1f679aff5955fd0bad16fdea9ca845e002b8
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-21 22:32:39 +0530

    Following Component updated

--- Components/Profile/Following.js
+++ Components/Profile/Following.js
@@ -1,5 +1,6 @@
 import CircularLoading from '../CircularLoading';
 import { useGetUserFollowing } from '../../apollo/actions';
+import PersonIcon from '@material-ui/icons/Person';
 
 
 export default function Following(props) {
@@ -11,12 +12,29 @@ export default function Following(props) {
 
     const userFollowing = data?.getUserFollowing?.userFollowingData ?? [];
 
-    console.log("userFollowing", userFollowing);
-
     return (
         <div>
             { loading && <CircularLoading />}
-            Following Component
+            <div className="follow_container">
+                {
+                    userFollowing.length > 0 &&
+                    userFollowing.map((userData, index) => (
+                        <div key={index}>
+                            <div className="follow_card_container">
+                                <div className="follow_card_avatar_background">
+                                    <PersonIcon className="follow_card_avatar" />
+                                </div>
+                                <div className="follow_card_user_info">
+                                    <div className="follow_card_user_name">{userData.userFollowingInfo.name}</div>
+                                </div>
+                                <button className="follow_card_unfollow_btn">unfollow</button>
+                            </div>
+                            { userFollowing.length - 1 > index && <hr className="follow_card_hr"></hr>}
+                        </div>
+                    ))
+                }
+                {error && <div></div>}
+            </div>
         </div>
     )
 }
