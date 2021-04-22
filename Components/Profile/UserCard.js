28b00e357a07ef6888aea69d45a815208eb33a97
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-22 21:22:39 +0530

    showing Edit or follow btn based on userID

--- Components/Profile/UserCard.js
+++ Components/Profile/UserCard.js
@@ -3,7 +3,7 @@ import PersonIcon from '@material-ui/icons/Person';
 
 export default function UserCard(props) {
 
-    const { userInfo, loading, error, setUserFollowType, userFollowType } = props;
+    const { userInfo, loginUserInfo, loading, setUserFollowType, userFollowType, showFollowBtn } = props;
 
     return (
         <div className="profile_page_sticky_column">
@@ -15,7 +15,12 @@ export default function UserCard(props) {
                     <div className="profile_user_info_container">
                         <div className="profile_card_user_name">{userInfo && userInfo.userData.username}</div>
                         {/* <div className="profile_card_name">{userInfo && userInfo.userData.name}</div> */}
-                        <button className="profile_card_edit_btn" onClick={() => setUserFollowType('editProfile')}>Edit Profile</button>
+                        {
+                            loginUserInfo && userInfo && loginUserInfo._id === userInfo.userData._id ?
+                                <button className="profile_card_edit_btn" onClick={() => setUserFollowType('editProfile')}>Edit Profile</button>
+                                :
+                                showFollowBtn && <button className="profile_card_edit_btn">Follow</button>
+                        }
                         <div className="profile_card_following_container">
                             <div className={`profile_card_followers ${userFollowType === 'follower' ? 'profile_active' : ''}`} onClick={() => setUserFollowType('follower')}>
                                 <span className={`profile_card_count`}>{userInfo && userInfo.followersCount}</span>
