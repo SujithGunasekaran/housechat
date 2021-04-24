33a18e3a5049c2fa43f759bb7e9eca823a597ebc
SujithGunasekaran <mydevelopstar@gmail.com>
2021-04-24 21:45:14 +0530

    followUser functionality implemented in client

--- Components/Profile/UserCard.js
+++ Components/Profile/UserCard.js
@@ -1,9 +1,16 @@
 import PersonIcon from '@material-ui/icons/Person';
-
+import { useFollowUser } from '../../apollo/actions';
 
 export default function UserCard(props) {
 
     const { userInfo, loginUserInfo, loading, setUserFollowType, userFollowType } = props;
+
+    // mutations
+    const [followUser, { data: followData, loading: followLoading, error: followError }] = useFollowUser();
+
+    const handleFollow = (loggedInUserId, followerId) => {
+        followUser({ variables: { userInfo: loggedInUserId, userFollowingInfo: followerId } });
+    }
 
     return (
         <div className="profile_page_sticky_column">
@@ -19,7 +26,7 @@ export default function UserCard(props) {
                             loginUserInfo && userInfo && loginUserInfo._id === userInfo.userData._id ?
                                 <button className="profile_card_edit_btn" onClick={() => setUserFollowType('editProfile')}>Edit Profile</button>
                                 :
-                                userInfo && userInfo.showFollow && <button className="profile_card_edit_btn">Follow</button>
+                                userInfo && userInfo.showFollow && <button className="profile_card_edit_btn" onClick={() => handleFollow(loginUserInfo._id, userInfo.userData._id)}>Follow</button>
                         }
                         <div className="profile_card_following_container">
                             <div className={`profile_card_followers ${userFollowType === 'follower' ? 'profile_active' : ''}`} onClick={() => setUserFollowType('follower')}>
@@ -32,9 +39,9 @@ export default function UserCard(props) {
                             </div>
                         </div>
                     </div>
-
                 </div>
             </div>
+            { followError && <div></div>}
         </div>
     )
 }
