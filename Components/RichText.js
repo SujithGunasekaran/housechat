b360fc923bfdeb87be396f3ab36a63786ae1c13f
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-02 11:09:47 +0530

    Setting inlineToolbar to false

--- Components/RichText.js
+++ Components/RichText.js
@@ -1,13 +1,12 @@
 import MUIRichTextEditor from "mui-rte";
 
 export default function RichText({ handleRichText, setCommentValue = null, defaultData }) {
-
     return (
         <MUIRichTextEditor
             label="Add Your Comments.."
             defaultValue={defaultData}
             onSave={handleRichText('content', setCommentValue)}
-            inlineToolbar={true}
+            inlineToolbar={false}
         />
     )
 }
