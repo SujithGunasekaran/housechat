adf8bc0ff5b18ff4410c2c0dba0959a4320e5918
SujithGunasekaran <mydevelopstar@gmail.com>
2021-05-02 19:35:32 +0530

    hoc components added

---
+++ hoc/withApollo.js
@@ -0,0 +1,41 @@
+import withApollo from 'next-with-apollo';
+import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
+import moment from 'moment';
+
+export default withApollo(
+    ({ initialState, headers }) => {
+        return new ApolloClient({
+            // uri: 'http://localhost:3000/graphql',
+            ssrMode: true,
+            link: createHttpLink({
+                uri: process.env.BASE_URL,
+                credentials: 'same-origin',
+                headers: {
+                    ...headers
+                },
+            }),
+            cache: new InMemoryCache().restore(initialState || {}),
+            resolvers: {
+                Portfolio: {
+                    daysOfExperience(data, args, context) { // context parameter we can get cache
+                        const { startDate, endDate } = data;
+                        let now = moment().unix();
+                        if (endDate) {
+                            now = endDate / 1000;
+                        }
+                        return moment.unix(now).diff(moment.unix(startDate / 1000), 'days');
+                    }
+                }
+            }
+        });
+    },
+    {
+        render: ({ Page, props }) => {
+            return (
+                <ApolloProvider client={props.apollo}>
+                    <Page {...props} />
+                </ApolloProvider>
+            );
+        }
+    }
+);
---
+++ hoc/withAuth.js
@@ -0,0 +1,59 @@
+import { useGetUser } from '../apollo/actions';
+import Redirect from '../Components/Redirect';
+import Loader from '../Components/Loader';
+
+const withAuth = (Component, role, options = { ssr: false }) => {
+
+    function newComponent(props) {
+
+        const { data, loading, error } = useGetUser({ fetchPolicy: 'network-only' })
+
+        if (!loading && (data && !data.user || error) && typeof window !== 'undefined') {
+            return (
+                <Redirect path="/Login" query={{ message: 'NOT_AUTHENTICATED', type: 'Error' }} />
+            )
+        }
+
+        if (data && data.user) {
+            if (role && role.length > 0 && !role.includes(data.user.role)) {
+                return <Redirect path="/Login" query={{ message: 'NOT_AUTHORIZED', type: 'Error' }} />
+            }
+            return <Component {...props} />
+        }
+
+        return (
+            <div className="spinner_container">
+                <Loader />
+            </div>
+        )
+    }
+
+    if (options.ssr) {
+        newComponent.getInitialProps = async (context) => {
+            const serverRedirect = (res, redirectPagePath) => {
+                res.redirect(redirectPagePath);
+                res.end();
+                return {};
+            }
+            const { req, res } = context;
+            if (req) {
+                const { user } = req;
+                if (!user) {
+                    return serverRedirect(res, '/Login?message=NOT_AUTHENTICATED&type=Error');
+                }
+                if (role && role.length > 0 && user && !role.includes(user.role)) {
+                    return serverRedirect(res, '/Login?message=NOT_AUTHORIZED&Type=Error');
+                }
+            }
+
+            const pageProps = Component.getInitialProps && await Component.getInitialProps(context);
+            return { ...pageProps };
+        }
+    }
+
+    return newComponent;
+}
+
+
+
+export default withAuth;
