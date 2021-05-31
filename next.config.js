dbbdf765057fe504a2731fe927b22403a69ec424
Sujith1219 <68234378+SujithGunasekaran@users.noreply.github.com>
2021-05-31 09:23:21 +0530

    Domain name change

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ const dev = process.env.NODE_ENV !== 'production';
 
 module.exports = {
     env: {
-        BASE_URL: dev ? 'http://localhost:3000/graphql' : 'https://housechat.herokuapp.com/graphql'
+        BASE_URL: dev ? 'http://localhost:3000/graphql' : 'https://forumhouse.herokuapp.com/graphql'
     }
 }
